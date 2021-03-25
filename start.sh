d7b850db2f15a4fa2a967263f7f137c2a4e9a845
root <root@ip-172-31-41-128.eu-central-1.compute.internal>
2021-03-25 11:25:52 +0000

    uncommented

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#yum install docker
-#docker pull flashspys/nginx-static
+yum install docker
+docker pull flashspys/nginx-static
 gdzie=$(pwd)
 docker run -v $gdzie/default.conf:/etc/nginx/conf.d/default.conf -p 8080:80 flashspys/nginx-static
