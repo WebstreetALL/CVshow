ac9f35b48ee0f7d7500631d00940667a156f6f2c
root <root@ip-172-31-33-141.eu-central-1.compute.internal>
2021-03-25 12:05:27 +0000

    port changed

--- start.sh
+++ start.sh
@@ -2,5 +2,6 @@
 
 yum install docker
 docker pull flashspys/nginx-static
+systemctl start docker
 gdzie=$(pwd)
-docker run -v $gdzie/default.conf:/etc/nginx/conf.d/default.conf -p 8080:80 flashspys/nginx-static
+docker run -v $gdzie/default.conf:/etc/nginx/conf.d/default.conf -p 80:80 flashspys/nginx-static
